dc90363ef398fb9d3cf6e46920aed4cf17188d6e
Andy DeAngelis <andy.deangelis@live.com>
2024-05-22 09:10:36 -0400

    updates

--- __main__.py
+++ __main__.py
@@ -17,16 +17,9 @@ if __name__ == "__main__":
     else:
         
         config_file = Path("/data/comic_path_config.json")
-        if config_file.is_file():
-            with open(config_file) as f_in:
-                current_root_path = (json.load(f_in))['comic_dir']
-
         api_file = Path("/data/comicvine_api_key.json")
-        if api_file.is_file():
-            with open(api_file) as f_in:
-                current_api_key = (json.load(f_in))['comic_api_key']
 
-        if current_root_path is not None and current_ap_key is not None:
+        if config_file.is_file() and api_file.is_file():
             GlobalFunctions.mainMenu()
         else:
             GlobalFunctions.modifySettingsMenu()                
