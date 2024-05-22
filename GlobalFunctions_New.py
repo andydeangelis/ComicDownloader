4f3f0055683d6aae5599740a97fcc08522d14d81
Andy DeAngelis <andy.deangelis@live.com>
2024-05-22 15:43:26 -0400

    updates

--- GlobalFunctions_New.py
+++ GlobalFunctions_New.py
@@ -239,9 +239,10 @@ class GlobalFunctions:
                 # Set the name for the CBZ file.
                 cbz_name = (link.string).replace(":","").replace(" / ","").replace("/"," ").replace("-)",")").replace("%","")
                 fileExists = Path(rootPath + "/" + folder + "/" + cbz_name + ".cbz")
+                fileExists2 = Path(rootPath + "/" + value + "/" + cbz_name + ".cbz")
                 
                 # If the link exists, continue to the next issue. If not, download the issue.
-                if not fileExists.is_file():
+                if not fileExists.is_file() and not fileExists2.is_file():
                     
                     # Create our sesion to the issue and get the encoded html.               
                     comicChapterPage = sess.get(comicLink, headers=headers)
