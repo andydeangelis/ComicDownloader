2d7e152c98d47c39ceb47a4f4aad70c5230f15b1
Andy DeAngelis <andy.deangelis@live.com>
2024-05-22 07:15:20 -0400

    updates

--- cronscript.sh
+++ cronscript.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -ti --rm --name comicdownloader --volume=/home/andy/docker/comicdownloader/data:/data --volume=/mnt/books/comics2:/comics dalthakar/comicdownloader:ubuntu-dev "--silent"
+docker run -ti --rm --name comicdownloader --volume=/home/andy/docker/comicdownloader/data:/data --volume=/mnt/books/comics2:/comics dalthakar/comicdownloader:andy-dev "--silent"
