8daab3e1e1b108d654ace53839affe10e617289e
Andy DeAngelis <andy.deangelis@live.com>
2024-05-22 07:11:15 -0400

    updates

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker image rm dalthakar/comicdownloader:ubuntu-dev
-time docker build -t dalthakar/comicdownloader:ubuntu-dev -f dockerfile-ubuntu .
+time docker build -t dalthakar/comicdownloader:andy-dev -f dockerfile-ubuntu .
