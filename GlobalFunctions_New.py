865cefb8c4f2d289aa42e9891dd9ad0641f0823d
Andy DeAngelis <andy.deangelis@live.com>
2025-08-06 19:22:54 -0400

    updates

--- GlobalFunctions_New.py
+++ GlobalFunctions_New.py
@@ -370,9 +370,7 @@ class GlobalFunctions:
             selectedComic = comicList[(comicToRemove - 1)]
 
             comicList.pop((comicToRemove - 1))
-            with open(trackerJsonFile,'w') as file:
-                file.write('')
-
+            
             for comic in comicList:                
                 with open (trackerJsonFile, 'r+') as file:
                     file_content = json.load(file)                                
